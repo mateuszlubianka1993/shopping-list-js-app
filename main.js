d45099dddda5038a242129d58d6c947d57e598f1
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-08-21 15:42:03 +0200

    get data from firestore and display it in app

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
-};
+    const list = document.querySelector('.list');
+
+    const displayItems = (doc) => {
+        let item = document.createElement('li');
+        let name = document.createElement('div');
+        let quantity = document.createElement('div');
+
+        item.setAttribute('data-id', doc.id);
+        
+        name.innerText = doc.data().name;
+        quantity.innerText = doc.data().quantity;
+
+        item.appendChild(name);
+        item.appendChild(quantity);
+        list.appendChild(item);
+    };
+
+    dataBase.collection('things').get().then((snapshot)=>{
+        snapshot.docs.forEach(doc => {
+            displayItems(doc);
+        });
+    });
+});
