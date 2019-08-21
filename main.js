7f8912d7d1bcd62082b849c0718b9925c6b2b387
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-08-21 16:08:32 +0200

    add data to firestore

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const list = document.querySelector('.list');
+    const appForm = document.querySelector('.app-form')
 
     const displayItems = (doc) => {
         let item = document.createElement('li');
@@ -20,5 +21,16 @@ document.addEventListener("DOMContentLoaded", function () {
         snapshot.docs.forEach(doc => {
             displayItems(doc);
         });
+    });
+
+    appForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        dataBase.collection('things').add({
+            name: appForm.name.value,
+            quantity: appForm.quantity.value,
+            isBought: false
+        });
+        appForm.name.value = '';
+        appForm.quantity.value = '';
     });
 });
