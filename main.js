774e070ccdfade6507ef7fa100c66c9fa19ca33a
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-08-21 16:27:06 +0200

    add elements and ui classes to list item

--- main.js
+++ main.js
@@ -6,14 +6,28 @@ document.addEventListener("DOMContentLoaded", function () {
         let item = document.createElement('li');
         let name = document.createElement('div');
         let quantity = document.createElement('div');
+        let rightDiv = document.createElement('div');
+        let leftDiv = document.createElement('div');
+        let content = document.createElement('div');
+        let btn = document.createElement('button');
 
         item.setAttribute('data-id', doc.id);
+        item.setAttribute('class', 'item');
+        rightDiv.setAttribute('class', 'right floated content');
+        leftDiv.setAttribute('class', 'left floated content');
+        content.setAttribute('class', 'content');
+        btn.setAttribute('class', 'ui negative button');
         
         name.innerText = doc.data().name;
         quantity.innerText = doc.data().quantity;
+        btn.innerText = 'Delete';
 
-        item.appendChild(name);
-        item.appendChild(quantity);
+        rightDiv.appendChild(btn);
+        leftDiv.appendChild(name);
+        content.appendChild(quantity);
+        item.appendChild(rightDiv);
+        item.appendChild(leftDiv);
+        item.appendChild(content);
         list.appendChild(item);
     };
 
